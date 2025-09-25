081bfbc9d9b99e14747416a5369dd896a0851706
Anthony Reyes <s106515@ap.be>
2025-09-25 19:35:30 +0200

    Kleine wijzigingen: mapstructuur, .gitignore en afbeeldingen toegevoegd

--- GustoItaliano/jest.config.js
+++ GustoItaliano/jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],   // Alleen tests/ map
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',     // Transform TypeScript bestanden
+  },
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1' // Optioneel: alias voor imports uit src/
+  },
 };
--- GustoItaliano/src/index.ts
+++ GustoItaliano/src/index.ts
@@ -9,8 +9,8 @@ const app : Express = express();
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.resolve(process.cwd(), "public")));
+app.set("views", path.resolve(process.cwd(), "views"));
 
 app.set("port", process.env.PORT ?? 3000);
 
