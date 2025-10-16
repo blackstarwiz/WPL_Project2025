ec7d0ff20215955af90c651c9a9f42404a3d3420
jason meulemans [Blackstarwiz] <59977182+blackstarwiz@users.noreply.github.com>
2025-10-16 23:22:06 +0200

    aanpassingen index,menu,login
    heb aanpassingen gemaakt bij de verschillende css,ejs files om de werking te verbeteren.
    Ook heb ik js file gemaakt om de functie van het wisselen dan de div(forms) te wisselen
    
    scroll,menu,reviews,bestsellers,aside,aanmeld(form),footer

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+let overButton = document.querySelector(".anchor");
+
+overButton.addEventListener('click', () =>{
+    window.scrollTo({
+        top: 2450,
+        behavior: "smooth"
+    })
+})
+
